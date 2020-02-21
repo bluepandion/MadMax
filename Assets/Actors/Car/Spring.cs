86532cb859754d452b3365e23e107185c49ced4c
Toni Svenström / Desktop <mangaelf@hotmail.com>
2020-02-21 13:15:32 +0200

    Add a charactercontroller based car script and prototype level blocks

---
+++ Assets/Actors/Car/Spring.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring
+{    
+    const float start = 0.5f;
+
+    public float value = 0.0f;
+
+    private float k = 12.0f;
+    private float x = start;      
+    private float energy = 0.5f * 2.0f * (start * start);  
+  
+    
+    public float Update(float delta, float affectingForce)
+    {
+        
+        float e = 0.5f * k * (x * x) * delta;        
+        x += Mathf.Sqrt(Mathf.Abs((2.0f * e) / k));
+                
+        energy -= e;        
+
+        if (x != 0f) {
+            Debug.Log(Mathf.Floor(x * 10000.0f));
+        }
+        
+        value = x;
+        return value;
+    }
+}
