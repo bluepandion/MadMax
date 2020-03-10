5244d2b9c030285f67cddc341667ee7aae6a56d6
Toni Svenström / Desktop <mangaelf@hotmail.com>
2020-03-10 16:46:50 +0200

    Integrate player with menu system  - Separate car from player state and input  - Add pause state functionality  - Tweak postprocessing

--- Assets/Actors/Bullet/Bullet.cs
+++ Assets/Actors/Bullet/Bullet.cs
@@ -6,34 +6,46 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 240f;
     public float life = 1.0f;
-    
-    private Vector3 direction;
+
     private float lifeTime = 0f;
+    private Rigidbody rb;
+
+    private Vector3 speedVector;
 
     // Start is called before the first frame update
     void Start()
     {
-        direction = new Vector3(0f, 0f, speed);
+        rb = GetComponent<Rigidbody>();
         lifeTime = 0f;
+        if (rb) {
+            rb.velocity = new Vector3(0f, 0f, 0f);
+            rb.AddForce(speedVector, ForceMode.Impulse);
+        }
     }
 
-    // Update is called once per frame
+    public void SetAngle(Quaternion a)
+    {
+        Vector3 direction = new Vector3(0f, 0f, speed);
+        speedVector = (a * direction);
+    }
+
     void Update()
     {
         lifeTime += Time.deltaTime;
         if (lifeTime > life) {
             Kill();
         }
-        transform.position += (transform.rotation * direction * Time.deltaTime);
+        //transform.position += (transform.rotation * direction * Time.deltaTime);
     }
 
-    private void OnTriggerEnter(Collider other) 
+    private void OnCollisionEnter(Collision c)//Collider other)
     {
-        GameObject g = other.gameObject;
+        Debug.Log("Bullet collided");
+        //GameObject g = other.gameObject;
         Kill();
     }
 
-    public void Kill() 
+    public void Kill()
     {
         Destroy(gameObject);
         Debug.Log("Destroy bullet");
---
+++ Assets/Actors/Player.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject menu;
+    public CarCharacterController car;
+    public PlayerState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        car = car.GetComponent<CarCharacterController>();
+        state = new StateDriving(this);
+    }
+
+    void Update()
+    {
+        if (state != null) {
+            state.Update();
+        }
+    }
+
+    public class PlayerState
+    {
+        protected Player o;
+        protected String name = "";
+        protected PlayerState previous;
+
+        public PlayerState(Player owner) {
+            previous = owner.state;
+            o = owner;
+        }
+
+        public virtual void Enter() {}
+
+        public virtual void Update() {}
+
+        public virtual void Exit() {}
+
+        public void Return() {
+            Exit();
+            o.state = previous;
+            previous.Enter();
+        }
+
+        public void Change(PlayerState to) {
+            Exit();
+            o.state = to;
+            o.state.Enter();
+        }
+    }
+
+    public class StateDriving : PlayerState
+    {
+        public StateDriving (Player owner) : base (owner) {}
+
+        public override void Enter() {
+            Debug.Log("Player state Driving");
+        }
+
+        public override void Update() {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                o.car.Shoot();
+            }
+
+            o.car.HandlePhysics(
+                Input.GetAxis("Horizontal"),
+                Input.GetAxis("Vertical"));
+
+            if (Input.GetButtonDown("Cancel")) {
+                Change(new Player.StatePaused(o));
+            }
+        }
+    }
+
+    public class StateMelting : PlayerState
+    {
+        private float time = 0f;
+        private const float MELT_DURATION = 1.0f;
+
+        public StateMelting (Player owner) : base (owner) {}
+
+        public override void Enter() {
+            Debug.Log("Player state Melting");
+        }
+
+        public override void Update() {
+            o.car.Melt();
+            time += Time.deltaTime;
+            if (time > MELT_DURATION) {
+                Change(new Player.StateDead(o));
+            }
+        }
+    }
+
+    public class StateDead : PlayerState
+    {
+        public StateDead (Player owner) : base (owner) {}
+
+        public override void Enter() {
+            Debug.Log("Player state Dead");
+        }
+    }
+
+    public class StatePaused : PlayerState
+    {
+        public StatePaused (Player owner) : base (owner) {}
+
+        public override void Enter() {
+            Debug.Log("Player state Paused");
+            Time.timeScale = 0f;
+            o.menu.SetActive(true);
+        }
+
+        public override void Update() {
+            if (Input.GetButtonDown("Cancel")) {
+                Return();
+            }
+            if (!o.menu.activeInHierarchy) {
+                Return();
+            }
+        }
+
+        public override void Exit() {
+            Debug.Log("Player state Paused - Exit");
+            o.menu.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+}
